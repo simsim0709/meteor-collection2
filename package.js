3d5c3bef16769d6fbc6267e824ebf51f77fefa0d
Ilkwon Sim <sim@theloho.com>
2016-10-13 20:29:15 +0900

    change meteor-schema-index package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* global Package */
 
 Package.describe({
-  name: "aldeed:collection2",
+  name: "thelohoadmin:collection2",
   summary: "Automatic validation of insert and update operations on the client and server.",
   version: "2.10.0",
   git: "https://github.com/aldeed/meteor-collection2.git"
@@ -11,13 +11,13 @@ Package.onUse(function(api) {
   // Automatically include all packages for now
   api.use([
     'aldeed:collection2-core@1.2.0',
-    'aldeed:schema-index@1.1.0',
+    'thelohoadmin:schema-index@1.1.0',
     'aldeed:schema-deny@1.1.0',
   ]);
 
   api.imply([
     'aldeed:collection2-core',
-    'aldeed:schema-index',
+    'thelohoadmin:schema-index',
     'aldeed:schema-deny',
   ]);
 });
